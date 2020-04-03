ef2a017012587fd2971f39c65d22da8aaa26348b
Ian Lai <ian@fyianlai.com>
2020-04-03 18:46:59 +0100

    Use Go 1.13 (BETA) runtime

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ gcloud alpha functions deploy stackdriver-to-discord \
   --entry-point F \
   --memory 128MB \
   --region us-central1 \
-  --runtime go111 \
+  --runtime go113 \
   --env-vars-file .env.yaml \
   --trigger-http
